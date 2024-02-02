37aa28fc1409cf5fff165874effae51acf8854b3
Klemen Hribar <klemen.hribar@live.com>
2024-02-02 21:07:34 +0100

    project setup
    -initial commit project setup

---
+++ src/main/java/com/addresses/service/AddressesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.addresses.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressesServiceApplication.class, args);
+    }
+
+}
