8a8f52d9a8d724a5038de63bab5d916d5b93fc04
Klemen Hribar <klemen.hribar@live.com>
2024-02-08 10:16:21 +0100

    -added Swagger anotations
    -added Swagger anotations  for API response documentation

--- src/main/java/com/addresses/service/web/AddressAPIController.java
+++ src/main/java/com/addresses/service/web/AddressAPIController.java
@@ -5,6 +5,11 @@ import com.addresses.service.database.service.AddressService;
 import com.addresses.service.service.AddressDTOService;
 import com.addresses.service.service.AddressValidationService;
 import com.addresses.service.web.dto.AddressDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +34,15 @@ public class AddressAPIController {
     }
 
     @GetMapping("/user/{userId}")
+    @Operation(summary = "Get addresses by user ID",
+            description = "Retrieves a list of addresses associated with the specified user ID")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of addresses",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AddressDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Bad request, could not find addresses for the specified user ID",
+            content = @Content)
+    @Parameter(name = "userId", description = "User ID to retrieve addresses for", required = true)
+
     public ResponseEntity<?> getAddressesByUserId(@PathVariable Long userId) {
         try {
             return ResponseEntity.ok(addressDTOService.convertToDTOList(addressService.findAddressesByUserId(userId)));
@@ -38,26 +52,45 @@ public class AddressAPIController {
     }
 
     @PostMapping
+    @Operation(summary = "Save a new address",
+            description = "Saves a new address with the provided information. Validates postal code and post office combination.")
+    @ApiResponse(responseCode = "200", description = "Successfully saved the address",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AddressDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Bad request, error saving address or invalid postal code and post office combination",
+            content = @Content)
+    @ApiResponse(responseCode = "500", description = "Error saving address",
+            content = @Content)
     public ResponseEntity<?> saveAddress(@RequestBody AddressDTO addressDTO) {
         try {
             if (!addressValidationService.isValidPostalCodeCombination(
                     addressDTO.getPostalCode(), addressDTO.getPostOffice())) {
-                throw new IllegalArgumentException("Invalid postal code and post office combination.");
+                return ResponseEntity.badRequest().body("Invalid postal code and post office combination.");
             }
 
             Address savedAddress = addressService.saveAddress(addressDTOService.convertToEntity(addressDTO));
             return ResponseEntity.ok(addressDTOService.convertToDTO(savedAddress));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error saving address: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving address: " + e.getMessage());
         }
     }
 
     @PutMapping
+    @Operation(summary = "Update address")
+    @ApiResponse(responseCode = "200", description = "Address updated",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AddressDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid postal code and post office combination.",
+            content = @Content)
+    @ApiResponse(responseCode = "404", description = "Address not found",
+            content = @Content)
+    @ApiResponse(responseCode = "500", description = "Error updating address",
+            content = @Content)
     public ResponseEntity<?> updateAddress(@RequestBody AddressDTO addressDTO) {
         try {
             if (!addressValidationService.isValidPostalCodeCombination(
                     addressDTO.getPostalCode(), addressDTO.getPostOffice())) {
-                throw new IllegalArgumentException("Invalid postal code and post office combination.");
+                return ResponseEntity.badRequest().body("Invalid postal code and post office combination.");
             }
 
             Address updatedAddress = addressService.updateAddress(addressDTO);
@@ -66,11 +99,17 @@ public class AddressAPIController {
         } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error updating address: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating address: " + e.getMessage());
         }
     }
 
     @GetMapping("/{addressId}")
+    @Operation(summary = "Get an address by its ID")
+    @ApiResponse(responseCode = "200", description = "Found the address",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AddressDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Could not find address with the given ID",
+            content = @Content)
     public ResponseEntity<?> getAddressById(@PathVariable Long addressId) {
         try {
             return ResponseEntity.ok(addressDTOService.convertToDTO(addressService.findAddressById(addressId)));
@@ -80,6 +119,14 @@ public class AddressAPIController {
     }
 
     @DeleteMapping("/{addressId}")
+    @Operation(summary = "Delete an Address")
+    @ApiResponse(responseCode = "200", description = "Ok",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AddressDTO.class))})
+    @ApiResponse(responseCode = "404", description = "Address not found",
+            content = @Content)
+    @ApiResponse(responseCode = "500", description = "Error deleting address",
+            content = @Content)
     public ResponseEntity<?> deleteAddress(@PathVariable Long addressId) {
         try {
             addressService.deleteAddress(addressId);
--- src/main/java/com/addresses/service/web/UserAPIController.java
+++ src/main/java/com/addresses/service/web/UserAPIController.java
@@ -5,6 +5,10 @@ import com.addresses.service.database.entity.User;
 import com.addresses.service.database.service.UserService;
 import com.addresses.service.service.UserDTOService;
 import com.addresses.service.web.dto.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +31,14 @@ public class UserAPIController {
     }
 
     @PostMapping("/user")
+    @Operation(summary = "Save a new user", description = "Saves a new user with the provided information.")
+    @ApiResponse(responseCode = "200", description = "Successfully saved the user",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Bad request, error saving user",
+            content = @Content)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "UserDTO object containing the new user information", required = true,
+            content = @Content(schema = @Schema(implementation = UserDTO.class)))
     public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) {
         try {
             User savedUser = userService.saveUser(userDTOService.convertToEntity(userDTO));
